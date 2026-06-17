bf74c430adeb4573376d13831a6c563af56f2165
MrCode <161657676+Mr-Code-star@users.noreply.github.com>
2026-06-17 01:28:17 -0500

    Add Lesson2.java with basic variable and operator examples

---
+++ class-materials/Lesson2.java
@@ -0,0 +1,18 @@
+public class MyClass {
+  public static void main(String args[]) {
+    /** Variables **/
+    int edad = 15;
+    String nombre = "Ana";
+
+    System.out.println(edad);
+    System.out.println(nombre);
+    
+    /** Operadores Basicos **/
+    
+    int edad_ = 5;
+    int edadEnMeses = edad_ * 15;
+    System.out.println(edadEnMeses);
+    
+    
+  }
+}
